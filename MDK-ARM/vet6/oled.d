b5276f007e1cb02f471e0680bdf5bb3f841e8e46
collectsand <ay2873403@gmail.com>
2021-06-21 22:54:41 +0800

    add DMA mode for oled

--- MDK-ARM/vet6/oled.d
+++ MDK-ARM/vet6/oled.d
@@ -24,6 +24,9 @@ vet6/oled.o: ..\Core\Src\oled.c ..\Core\Inc\oled.h ..\Core\Inc\main.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_cortex.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_flash.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_flash_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_i2c.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_pwr.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_spi.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_tim.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_tim_ex.h \
   ..\Core\Inc\spi.h ..\Core\Inc\oledfonts.h
