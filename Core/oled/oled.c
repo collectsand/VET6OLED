15a8979cfbba178fc2833f3a49cd4eb9ebd8ca95
collectsand <ay2873403@gmail.com>
2021-07-24 15:06:36 +0800

    complete MPU6050 driver and update OLED driver

--- Core/oled/oled.c
+++ Core/oled/oled.c
@@ -1,4 +1,20 @@
+/*
+ __________________
+|__________________ P7                    _______________________________________  HSB       
+|__________________ P6
+|__________________ P5                   
+|__________________ P4                                                              ^      
+|__________________ P3                              P0                              ^
+|__________________ P2                                                              ^
+|__________________ P1                 
+|__________________ P0 -------->          ______________________________________   LSB   
+
+*/
+
 #include "oled.h"
+#include "spi.h"
+#include "oledfonts.h"
+#include "stdlib.h"
 
 uint8_t GRAM[1024] = {0};
 OLED_PoisitionStruct OLED_Poisition;
@@ -6,43 +22,50 @@ OLED_FPSStruct OLED_FPS;
 
 void OLED_Init()
 {
+    HAL_Delay(1000);
     OLED_Reset();
     HAL_Delay(100);
 
     OLED_WriteCmd(0xAE); //关闭显示
 
     OLED_WriteCmd(0x20); //寻址模式
-    OLED_WriteCmd(0x00); //水平寻址
+    OLED_WriteCmd(0x00);
+
+    OLED_WriteCmd(0xB0);
+    OLED_WriteCmd(0x00);
+    OLED_WriteCmd(0x10);
+    OLED_WriteCmd(0x40); //设置显示开始行 [5:0],行数.
 
     OLED_WriteCmd(0xAE); //关闭显示
     OLED_WriteCmd(0xD5); //设置时钟分频因子,震荡频率
-    OLED_WriteCmd(80);   //[3:0],分频因子;[7:4],震荡频率
+    OLED_WriteCmd(0xf0); //[3:0],分频因子;[7:4],震荡频率
 
     OLED_WriteCmd(0xA8); //设置驱动路数
     OLED_WriteCmd(0X3F); //默认0X3F(1/64)
 
     OLED_WriteCmd(0xD3); //设置显示偏移
     OLED_WriteCmd(0X00); //默认为0
-    OLED_WriteCmd(0x40); //设置显示开始行 [5:0],行数.
 
     OLED_WriteCmd(0x8D); //电荷泵设置
     OLED_WriteCmd(0x14); //bit2，开启/关闭
 
     OLED_WriteCmd(0xA1); //段重定义设置,bit0:0,0->0;1,0->127;
     OLED_WriteCmd(0xC0); //设置COM扫描方向;bit3:0,普通模式;1,重定义模式 COM[N-1]->COM0;N:驱动路数
+
     OLED_WriteCmd(0xDA); //设置COM硬件引脚配置
     OLED_WriteCmd(0x12); //[5:4]配置
 
     OLED_WriteCmd(0x81); //对比度设置
-    OLED_WriteCmd(0xEF); //1~255;默认0X7F (亮度设置,越大越亮)
+    OLED_WriteCmd(0xFF); //1~255;默认0X7F (亮度设置,越大越亮)
 
     OLED_WriteCmd(0xD9); //设置预充电周期
-    OLED_WriteCmd(0xf1); //[3:0],PHASE 1;[7:4],PHASE 2;
-    OLED_WriteCmd(0xDB); //设置VCOMH 电压倍率
-    OLED_WriteCmd(0x30); //[6:4] 000,0.65*vcc;001,0.77*vcc;011,0.83*vcc;
+    OLED_WriteCmd(0x22); //[3:0],PHASE 1;[7:4],PHASE 2;
 
-    OLED_WriteCmd(0xA4); //全局显示开启;bit0:1,开启;0,关闭;(白屏/黑屏)
+    OLED_WriteCmd(0xDB); //设置VCOMH 电压倍率
+    OLED_WriteCmd(0x20); //[6:4] 000,0.65*vcc;001,0.77*vcc;011,0.83*vcc;
+
     OLED_WriteCmd(0xA6); //设置显示方式;bit0:1,反相显示;0,正常显示
+    OLED_WriteCmd(0xA4); //全局显示开启;bit0:1,开启;0,关闭;(白屏/黑屏)
 
     OLED_WriteCmd(0xAF); //开启显示
 
@@ -106,12 +129,13 @@ void OLED_DrawPixel(uint8_t x, uint8_t y, uint8_t color)
 }
 
 //8x16
-void OLED_WriteChar(const uint8_t *ch, uint8_t x, uint8_t y)
+void OLED_WriteChar(uint8_t ch, uint8_t x, uint8_t y)
 {
-    uint8_t temp;
-    for (uint8_t i = 0; i < 8; i++)
+    uint8_t i, temp;
+    ch = ch - ' ';
+    for (i = 0; i < 8; i++)
     {
-        temp = ch[i];
+        temp = oled_ascll[16 * ch + i];
         for (uint8_t j = 0; j < 8; j++)
         {
             if ((temp << j) & 0x80)
@@ -122,9 +146,9 @@ void OLED_WriteChar(const uint8_t *ch, uint8_t x, uint8_t y)
     }
 
     y -= 8;
-    for (uint8_t i = 8; i < 16; i++)
+    for (i = 8; i < 16; i++)
     {
-        temp = ch[i];
+        temp = oled_ascll[16 * ch + i];
         for (uint8_t j = 0; j < 8; j++)
         {
             if ((temp << j) & 0x80)
@@ -132,6 +156,39 @@ void OLED_WriteChar(const uint8_t *ch, uint8_t x, uint8_t y)
             else
                 OLED_DrawPixel(x + i - 8, y + j, Black);
         }
+    }
+}
+
+void OLED_WriteString(char *str, uint8_t x, uint8_t y)
+{
+    while (*str != '\0')
+    {
+        OLED_WriteChar(*str, x, y);
+        str++;
+        x += 8;
+    }
+}
+
+void OLED_WriteNumber(int number, uint8_t x, uint8_t y)
+{
+    uint8_t h, m, l;
+    h = abs(number) / 100;
+    m = (abs(number) / 10) % 10;
+    l = abs(number) % 10;
+
+    if (number > 0)
+    {
+        OLED_WriteChar(' ', x, y);
+        OLED_WriteChar(h + 16 + ' ', x + 8, y);
+        OLED_WriteChar(m + 16 + ' ', x + 16, y);
+        OLED_WriteChar(l + 16 + ' ', x + 24, y);
+    }
+    else
+    {
+        OLED_WriteChar('-', x, y);
+        OLED_WriteChar(h + 16 + ' ', x + 8, y);
+        OLED_WriteChar(m + 16 + ' ', x + 16, y);
+        OLED_WriteChar(l + 16 + ' ', x + 24, y);
     }
 }
 
---
+++ MDK-ARM/vet6/inv_mpu_dmp_motion_driver.d
@@ -0,0 +1,38 @@
+vet6/inv_mpu_dmp_motion_driver.o: \
+  ..\Core\MPU6050\inv_mpu_dmp_motion_driver.c \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  ..\Core\MPU6050\inv_mpu.h ..\Core\MPU6050\inv_mpu_dmp_motion_driver.h \
+  ..\Core\MPU6050\dmpKey.h ..\Core\MPU6050\dmpmap.h \
+  ..\Core\MPU6050\mpuI2C.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal.h \
+  ..\Core\Inc\stm32f1xx_hal_conf.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_rcc.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_def.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\stm32f1xx.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\stm32f103xe.h \
+  ..\Drivers\CMSIS\Include\core_cm3.h \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\Drivers\CMSIS\Include\cmsis_version.h \
+  ..\Drivers\CMSIS\Include\cmsis_compiler.h \
+  ..\Drivers\CMSIS\Include\cmsis_armclang.h \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\system_stm32f1xx.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_rcc_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_gpio.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_gpio_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_exti.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_dma.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_dma_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_cortex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_flash.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_flash_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_i2c.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_pwr.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_spi.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_tim.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_tim_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_uart.h \
+  d:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h
